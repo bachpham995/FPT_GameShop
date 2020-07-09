e5ad2d342b85bc9017de1306a009fffb5c2dd9c0
nguyenvuhoanghoa95 <48507433+nguyenvuhoanghoa95@users.noreply.github.com>
2020-07-10 03:48:12 +0700

    commit login
    them login tu master

---
+++ eProject_HK2/resources/views/admin/member/home.blade.php
@@ -0,0 +1,105 @@
+<!-- Lưu tại resources/views/product/index.blade.php -->
+@extends('admin.layout')
+@section('title', 'User')
+@section('content')
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>DataTables</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">DataTables</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">DataTable with minimal features & hover style</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <table id="product" class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>User ID</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Address</th>
+                                <th>Phone</th>
+                                <th>Type</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $u)
+                            <tr>
+                                <td>{{ $u->ID }}</td>
+                                <td>{{ $u->FNAME }}</td>
+                                <td>{{ $u->LNAME }}</td>
+                                <td>{{ $u->EMAIL }}</td>
+                                <td>{{ $u->ADDRESS }}</td>
+                                <td>{{ $u->PHONE }}</td>
+                                <td>{{ $u->TYPE==1?"Admin":"User" }}</td>
+                                <td class="text-right">
+                                    <a class="btn btn-primary btn-sm" href="#">
+                                        <i class="fas fa-folder"></i> View
+                                    </a>
+                                    <a class="btn btn-info btn-sm" href="{{ url('product/update/'.$u->ID) }}">
+                                        <i class="fas fa-pencil-alt"></i> Edit
+                                    </a>
+                                    <a class="btn btn-danger btn-sm" href="{{ url('product/delete/'.$u->ID) }}">
+                                        <i class="fas fa-trash"></i> Delete
+                                    </a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>User ID</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Address</th>
+                                <th>Phone</th>
+                                <th>Type</th>
+                                <th></th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+</section>
+@endsection
+@section('script-section')
+<script>
+    $(function() {
+        $('#product').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+        });
+    });
+</script>
+@endsection
