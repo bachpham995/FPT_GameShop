70d3e66167324a8e72f058f0e2c11bb89eabe452
Pham Xuan Bach <bachpham995@gmail.com>
2020-07-15 18:27:18 +0700

    commit all

--- eProject_HK2/resources/views/admin/main-sidebar.blade.php
+++ eProject_HK2/resources/views/admin/main-sidebar.blade.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
         <img src="{{ asset('img/AdminLTELogo.png') }}" alt="Product Demo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Product Demo</span>
+        <span class="brand-text font-weight-light text-light">Product Demo</span>
     </a>
 
     <!-- Sidebar -->
@@ -13,7 +13,7 @@
                 <img src="{{ asset('img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Admin</a>
+                <a href="#" class="d-block text-light">Admin</a>
             </div>
         </div>
 
@@ -22,8 +22,8 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
-                <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
+                <li class="nav-item has-treeview ">
+                    <a href="#" class="nav-link text-light">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Products
