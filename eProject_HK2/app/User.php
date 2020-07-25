ea2bd8b2ab4600b96b4ea23612e7ea81589a42f6
Phat Luu Trong <phat.luu@capgemini.com>
2020-07-25 15:48:01 +0700

    Fix ADMIN SITE bugs

--- eProject_HK2/app/User.php
+++ eProject_HK2/app/User.php
@@ -8,4 +8,28 @@ class user extends Model
 {
     protected $table = "user";
     protected $fillable = ['TYPE','FNAME','EMAIL','PASSWORD','ADDRESS','PHONE','updated_at','created_at'];
+
+    function UserType(){
+        switch ($this->TYPE) {
+            case 0:
+                return "Supervisor";
+                break;
+
+            case 1:
+                return "Admin";
+                break;
+
+            case 2:
+                return "User";
+                break;
+
+            default:
+                # code...
+                break;
+        }
+    }
+
+    function CurrentUser(){
+        return auth()->user();
+    }
 }
