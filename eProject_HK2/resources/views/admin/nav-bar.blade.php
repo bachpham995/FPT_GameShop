77b7774e0e26d41a673fc6182fa6d81f14755357
Phat Luu Trong <phat.luu@capgemini.com>
2020-07-25 18:37:00 +0700

    Small fix

--- eProject_HK2/resources/views/admin/nav-bar.blade.php
+++ eProject_HK2/resources/views/admin/nav-bar.blade.php
@@ -5,14 +5,14 @@
             <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{ url('/') }}" class="nav-link">Home</a>
+            <a href="{{ url('admin/products/home') }}" class="nav-link">Home</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
             <a href="#" class="nav-link">Contact</a>
         </li>
     </ul>
     <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
+    {{-- <form class="form-inline ml-3">
         <div class="input-group input-group-sm">
             <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
             <div class="input-group-append">
@@ -21,5 +21,5 @@
                 </button>
             </div>
         </div>
-    </form>
+    </form> --}}
 </nav>
