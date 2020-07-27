f9af2f41ebbf51d4141480575fc83a7a7a594a66
Pham Xuan Bach <bachpham995@gmail.com>
2020-07-27 18:52:27 +0700

    fixbug

--- eProject_HK2/resources/views/client/index.blade.php
+++ eProject_HK2/resources/views/client/index.blade.php
@@ -1,5 +1,89 @@
-@extends('layout.layout')
-@section('title', 'Homepage')
-@section('content')
-    <h1>This is home page</h1>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link href="https://fonts.googleapis.com/css?family=Hind:400,700" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/client/bootstrap.min.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ asset('css/client/slick.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ asset('css/client/slick-theme.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ asset('css/client/nouislider.min.css') }}" />
+    {{-- <link rel="stylesheet" href="{{ asset('css/client/font-awesome.min.css') }}"> --}}
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/client/style.css') }}" />
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/client/login.css') }}" />
+</head>
+<body>
+    <!-- HEADER -->
+    @include('layout.header')
+    <!-- /HEADER-->
+
+    <!-- NAVIGATION -->
+    @include('layout.navigation')
+    <!-- /NAVIGATION-->
+
+	<!-- HOME -->
+	<div id="home">
+		<!-- container -->
+		<div class="container">
+			<!-- home wrap -->
+			<div class="home-wrap">
+				<!-- home slick -->
+				<div id="home-slick">
+					<!-- banner -->
+					<div class="banner banner-1">
+						<img src="./img/banner01.jpg" alt="">
+						<div class="banner-caption text-center">
+							<h1>Bags sale</h1>
+							<h3 class="white-color font-weak">Up to 50% Discount</h3>
+							<button class="primary-btn">Shop Now</button>
+						</div>
+					</div>
+					<!-- /banner -->
+
+					<!-- banner -->
+					<div class="banner banner-1">
+						<img src="./img/banner02.jpg" alt="">
+						<div class="banner-caption">
+							<h1 class="primary-color">HOT DEAL<br><span class="white-color font-weak">Up to 50% OFF</span></h1>
+							<button class="primary-btn">Shop Now</button>
+						</div>
+					</div>
+					<!-- /banner -->
+
+					<!-- banner -->
+					<div class="banner banner-1">
+						<img src="./img/banner03.jpg" alt="">
+						<div class="banner-caption">
+							<h1 class="white-color">New Product <span>Collection</span></h1>
+							<button class="primary-btn">Shop Now</button>
+						</div>
+					</div>
+					<!-- /banner -->
+				</div>
+				<!-- /home slick -->
+			</div>
+			<!-- /home wrap -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /HOME -->
+
+    <!-- FOOTER -->
+    @include('layout.footer')
+    <!-- /FOOTER-->
+    <!-- -->
+
+    <script src="{{ asset('js/client/jquery.min.js') }}"></script>
+	<script src="{{ asset('js/client/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('js/client/slick.min.js') }}"></script>
+	<script src="{{ asset('js/client/nouislider.min.js') }}"></script>
+	<script src="{{ asset('js/client/jquery.zoom.min.js') }}"></script>
+    <script src="{{ asset('js/client/main.js') }}"></script>
+    
+    <!-- page script -->
+    @yield('script-section')
+</body>
+</html>
