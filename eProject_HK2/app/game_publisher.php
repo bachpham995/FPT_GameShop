466528d3b52a6b287f543ab801199db620cfb936
nguyenvuhoanghoa <nguyenvuhoanghoa95@gmail.com>
2020-08-09 16:34:23 +0700

    commit update about us  and bill

--- eProject_HK2/app/game_publisher.php
+++ eProject_HK2/app/game_publisher.php
@@ -8,4 +8,12 @@ class game_publisher extends Model
 {
     protected $table = "game_publisher";
     public $incrementing = false;
+    public function game(){
+        $game = game::find($this->GAME_ID);
+        return $game;
+    }
+    public function publisher(){
+        $publisher = publisher::find($this->PUBLISHER_ID);
+        return $publisher;
+    }
 }
